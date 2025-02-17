c24fecd396e351886a570add1cc4b2c01f5c8b7e
Katarzyna Werner <katarzyna.werner1234@gmail.com>
2025-02-17 11:28:55 +0100

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.es-swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.es-swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.es-swiper-button-next',
+    prevEl: '.es-swiper-button-prev',
+  },
+
+});
